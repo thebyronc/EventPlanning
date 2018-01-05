98f043b1966721858e0a60b3a9c42006b0376e4b
Byron Chang <thebyronc@gmail.com>
2018-01-05 10:57:14 -0800

    add failing test for get number of guests

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,6 +1,15 @@
 package models;
 
 public class Event {
+    public String name;
+    public int guests;
+    public String food;
+    public String drinks;
+    public String entertainment;
+
+    public static int getGuests(Event event) {
+        return 2;
+    }
     public static String checkUserResponse(String userInput) {
         String input = userInput.toLowerCase();
         switch (input) {
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -15,4 +15,15 @@ public class EventTest {
         Event event = new Event();
         assertEquals("Im sorry, I do not understand.", event.checkUserResponse("@5K"));
     }
+    @Test
+    public void Event_getEventGuestNumber() {
+        Event event = new Event();
+        Event test = new Event();
+        test.name = "Test Event Name";
+        test.guests = 3;
+        test.drinks = "Pizza";
+        test.drinks = "Coffee";
+        test.entertainment = "Band";
+        assertEquals(3, event.getGuests(test));
+    }
 }
