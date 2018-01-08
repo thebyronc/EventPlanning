b1d0922d29608c495869f6b47a7b902256fd7d32
Byron Chang <thebyronc@gmail.com>
2018-01-07 16:03:58 -0800

    update testing to use encapsulated sections

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -8,91 +8,49 @@ public class EventTest {
 
     @Test
     public void Event_checkUserResponse() {
-        Event event = new Event();
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
         assertEquals("yes", event.checkUserResponse("yes"));
     }
     @Test
     public void Event_checkInvalidResponse() {
-        Event event = new Event();
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
         assertEquals("Im sorry, I do not understand.", event.checkUserResponse("@5K"));
     }
     @Test
     public void Event_getEventGuestNumber() {
-        Event event = new Event();
-        Event test = new Event();
-        test.name = "Test Event Name";
-        test.guests = 3;
-        test.food = "Pizza";
-        test.drinks = "Coffee";
-        test.entertainment = "Band";
-        assertEquals(3, event.getGuests(test));
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
+        assertEquals(3, event.getGuests(event));
     }
     @Test
     public void Event_getFood() {
-        Event event = new Event();
-        Event test = new Event();
-        test.name = "Test Event Name";
-        test.guests = 3;
-        test.food = "Pizza";
-        test.drinks = "Coffee";
-        test.entertainment = "Band";
-        assertEquals("Pizza", event.getFood(test));
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
+        assertEquals("Pizza", event.getFood(event));
     }
     @Test
     public void Event_getDrink() {
-        Event event = new Event();
-        Event test = new Event();
-        test.name = "Test Event Name";
-        test.guests = 3;
-        test.food = "Pizza";
-        test.drinks = "Coffee";
-        test.entertainment = "Band";
-        assertEquals("Coffee", event.getDrink(test));
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
+        assertEquals("Coffee", event.getDrink(event));
     }
     @Test
     public void Event_getEntertainment() {
-        Event event = new Event();
-        Event test = new Event();
-        test.name = "Test Event Name";
-        test.guests = 3;
-        test.food = "Pizza";
-        test.drinks = "Coffee";
-        test.entertainment = "Band";
-        assertEquals("Band", event.getEntertainment(test));
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
+        assertEquals("Band", event.getEntertainment(event));
     }
     @Test
     public void Event_getEventName() {
-        Event event = new Event();
-        Event test = new Event();
-        test.name = "Test Event Name";
-        test.guests = 3;
-        test.food = "Pizza";
-        test.drinks = "Coffee";
-        test.entertainment = "Band";
-        assertEquals("Test Event Name", event.getEventName(test));
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
+        assertEquals("Test Event Name", event.getEventName(event));
     }
     @Test
     public void Event_calculateCost() {
-        Event event = new Event();
-        Event test = new Event();
-        test.name = "Test Event Name";
-        test.guests = 3;
-        test.food = "Pizza";
-        test.drinks = "Coffee";
-        test.entertainment = "Band";
-        assertEquals(230, event.calculateCost(test));
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
+        assertEquals(230, event.calculateCost(event));
     }
     @Test
     public void Event_useCoupons() {
-        Event event = new Event();
-        Event test = new Event();
-        test.name = "Test Event Name";
-        test.guests = 3;
-        test.food = "Pizza";
-        test.drinks = "Coffee";
-        test.entertainment = "Band";
+        Event event = new Event("Test Event Name", 3, "Pizza", "Coffee", "Band");
         String[] coupons = {"FreeDJ", "25Off"};
-        assertEquals(5, event.useCoupons(test, coupons));
+        assertEquals(5, event.useCoupons(event, coupons));
     }
 
 }
